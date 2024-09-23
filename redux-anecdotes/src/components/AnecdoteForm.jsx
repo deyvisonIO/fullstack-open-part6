3ba53448db173f061df42f7b8f11896c0905dab2
Deyvison Iury <iurydeyvison@gmail.com>
2024-09-23 14:53:40 -0300

    Exercises 6.3-6.8

---
+++ redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,29 @@
+import { useDispatch } from "react-redux";
+import { createAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+
+  const handleAnecdoteCreation = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const anecdote = formData.get("anecdote");
+
+    if (!anecdote) return;
+
+    dispatch(createAnecdote(anecdote));
+  };
+
+  return (
+    <form onSubmit={handleAnecdoteCreation}>
+      <h2>create new</h2>
+      <div>
+        <input type="text" name="anecdote" />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  );
+};
+
+export default AnecdoteForm
